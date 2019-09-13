b4277cae88f2e0f9dd370cdfd971bb41cf9699e2
angel_yy <angel_yy@126.com>
2019-09-13 22:29:11 +0800

    pygame21

--- 2嵩天Python教程/2游戏开发/PYG03/PYG03-PygameWallBallv7.py
+++ 2嵩天Python教程/2游戏开发/PYG03/PYG03-PygameWallBallv7.py
@@ -1,12 +1,17 @@
-# Unit PYG02: Pygame Wall Ball Game Version 1
+# Unit PYG02: Pygame Wall Ball Game Version 7
 import pygame
 import sys
 
 pygame.init()
+icon = pygame.image.load("PYG03-flower.png")
+pygame.display.set_icon(icon)
 size = width, height = 600, 400
 speed = [1, 1]
 BLACK = 0, 0, 0
 screen = pygame.display.set_mode(size)
+# screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+# screen = pygame.display.set_mode(size, pygame.NOFRAME)
+# screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
 pygame.display.set_caption("Pygame壁球")
 ball = pygame.image.load("PYG02-ball.gif")
 ballrect = ball.get_rect()
@@ -26,7 +31,13 @@ while True:
                 speed[1] = speed[1] + 1 if speed[1] > 0 else speed[1] - 1
             elif event.key == pygame.K_DOWN:
                 speed[1] = speed[1] if speed[1] == 0 else (abs(speed[1]) - 1) * int(speed[1]/abs(speed[1]))
-    ballrect = ballrect.move(speed[0], speed[1])
+            elif event.key == pygame.K_ESCAPE:
+                sys.exit()
+        elif event.type == pygame.RESIZABLE:
+            size = width, height = event.size[0], event.size[1]
+            screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+    if pygame.display.get_active():
+        ballrect = ballrect.move(speed[0], speed[1])
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
     if ballrect.top < 0 or ballrect.bottom > height:
